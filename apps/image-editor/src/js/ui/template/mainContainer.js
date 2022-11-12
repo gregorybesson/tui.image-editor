e9bed71caaaf7f1061d11a298a5eab52c06c66fa
greg <bessong@gmail.com>
2022-11-12 08:41:52 +0100

    drag drop

--- apps/image-editor/src/js/ui/template/mainContainer.js
+++ apps/image-editor/src/js/ui/template/mainContainer.js
@@ -28,25 +28,25 @@ export default ({
             </div>
             <div class="tui-image-editor-jira" style="position: absolute; right: 10px; background-color: #fff; width: 500px; height: 100%; z-index: 2;">
                 <form id="formJira">
-                    <input type="hidden" id="jira-page-title"/>
-                    <input type="hidden" id="jira-page-url"/>
+                    <input type="hidden" name="page-title" id="jira-page-title"/>
+                    <input type="hidden" name="page-url" id="jira-page-url"/>
                     <div class="form-group">
                         <label for="jiraProjects">Project <superscript>*</superscript></label>
-                        <select id="jiraProjects" style="user-select: auto;" required></select>
+                        <select name="key" id="jiraProjects" style="user-select: auto;" required></select>
                     </div>
                     <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="keepProjectKey" checked="checked">
+                        <input type="checkbox" class="form-check-input" name="keep-project" id="keepProjectKey" checked="checked">
                         <label class="form-check-label" for="keepProjectKey">Keep this project for this website</label>
                     </div>
                     <div class="form-group">
                         <br>
                         <label for="jira-summary">Summary <superscript>*</superscript></label>
-                        <input type="text" class="form-control" id="jira-summary" placeholder="Short description" required>
+                        <input type="text" class="form-control" name="summary" id="jira-summary" placeholder="Short description" required>
                         <small id="summaryHelp" class="form-text text-muted">Enter a short description of your ticket</small>
                     </div>
                     <div class="form-group">
                         <label for="jira-description">Description <superscript>*</superscript></label>
-                        <textarea class="form-control" id="jira-description" rows="5" style="user-select: auto;" required></textarea>
+                        <textarea class="form-control" name="description" id="jira-description" rows="5" style="user-select: auto;" required></textarea>
                         <small id="descriptionHelp" class="form-text text-muted">try to be specific. Your description will be appended with your screenshot</small>
                     </div>
                     <button type="submit" title="Create a ticket" class="btn btn-primary share">Send to Jira</button>
